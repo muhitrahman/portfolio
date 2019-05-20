4ed2f0a428fc2fabf368e712effa7ba461e7635e
Muhit <36333637+muhitrahman@users.noreply.github.com>
2019-05-20 16:03:25 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+$(document).ready(function(){
+    $("#imgCarousel").carousel();
+});
+
+
+$(function(){
+    $('.dropdown').hover(function() {
+        $(this).addClass('open');
+    },
+    function() {
+        $(this).removeClass('open');
+    });
+});
+
+
+
